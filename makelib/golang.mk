36c039150683eca1b69889b2f4cafec3252edf8f
ichekrygin <illya.chekrygin@gmail.com>
2018-09-14 13:51:00 -0700

    Add go.generate so we can generate code via `go generate`

--- makelib/golang.mk
+++ makelib/golang.mk
@@ -181,7 +181,13 @@ go.clean:
 go.distclean:
 	@rm -rf $(GO_VENDOR_DIR) $(GO_PKG_DIR)
 
-.PHONY: go.init go.build go.install go.test.unit go.test.integration go.lint go.vet go.fmt
+go.generate:
+	@$(INFO) go generate $(PLATFORM)
+	@CGO_ENABLED=0 $(GOHOST) generate $(GO_COMMON_FLAGS) $(GO_PACKAGES) $(GO_INTEGRATION_TEST_PACKAGES) || $(FAIL)
+	@$(OK) go generate $(PLATFORM)
+
+
+.PHONY: go.init go.build go.install go.test.unit go.test.integration go.lint go.vet go.fmt go.generate
 .PHONY: go.validate go.vendor.lite go.vendor go.clean go.distclean
 
 # ====================================================================================
@@ -205,6 +211,7 @@ vet: go.vet
 
 define GO_HELPTEXT
 Go Targets:
+    generate     Runs go code generation.
     fmt          Checks go source code for formatting issues.
     vendor       Updates vendor packages.
     vet          Checks go source code and reports suspicious constructs.
