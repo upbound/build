285a66e0ec9db5d5cf1cfd075f714b742fda269e
Michael Goff <thephred@gmail.com>
2018-11-21 09:25:18 -0800

    Make the publishing of docs more flexible.

--- makelib/docs.mk
+++ makelib/docs.mk
@@ -1,35 +1,35 @@
 # ====================================================================================
 # Options
 
-DOCS_VERSION := $(shell echo $(BRANCH_NAME) | sed -E "s/^release\-([0-9]+)\.([0-9]+)$$/v\1.\2/g")
-DOCS_DIR ?= $(ROOT_DIR)/Documentation
-DOCS_WORK_DIR := $(WORK_DIR)/upbound.github.io
-DOCS_VERSION_DIR := $(DOCS_WORK_DIR)/docs/upbound/$(DOCS_VERSION)
-
-ifdef GIT_API_TOKEN
-DOCS_GIT_REPO := https://$(GIT_API_TOKEN)@github.com/upbound/upbound.github.io.git
-else
-DOCS_GIT_REPO := git@github.com:upbound/upbound.github.io.git
+ifndef SOURCE_DOCS_DIR
+$(error SOURCE_DOCS_DIR must be defined)
 endif
+
+ifndef DEST_DOCS_DIR
+$(error DEST_DOCS_DIR must be defined)
+endif
+
+ifndef DOCS_GIT_REPO
+$(error DOCS_GIT_REPO must be defined)
+endif
+
+DOCS_VERSION := $(shell echo $(BRANCH_NAME) | sed -E "s/^release\-([0-9]+)\.([0-9]+)$$/v\1.\2/g")
+DOCS_WORK_DIR := $(WORK_DIR)/docs-repo
+DOCS_VERSION_DIR := $(DOCS_WORK_DIR)/$(DEST_DOCS_DIR)/$(DOCS_VERSION)
 
 # ====================================================================================
 # Targets
 
-docs.build:
+docs.publish:
 	rm -rf $(DOCS_WORK_DIR)
 	mkdir -p $(DOCS_WORK_DIR)
 	git clone --depth=1 -b master $(DOCS_GIT_REPO) $(DOCS_WORK_DIR)
 	rm -rf $(DOCS_VERSION_DIR)
-	cp -r $(DOCS_DIR)/ $(DOCS_VERSION_DIR)
-	cd $(DOCS_WORK_DIR) && npm install && node build/scripts/preprocess.js
-
-docs.publish:
-	$(DOCS_WORK_DIR)/build/publish.sh
+	cp -r $(SOURCE_DOCS_DIR)/ $(DOCS_VERSION_DIR)
+	cd $(DOCS_WORK_DIR) && DOCS_VERSION=$(DOCS_VERSION) $(MAKE) publish
 
 # ====================================================================================
 # Common Targets
-
-build.artifacts: docs.build
 
 # only publish docs for master and release branches
 ifneq ($(filter master release-%,$(BRANCH_NAME)),)
