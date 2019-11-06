1447a3ce75f9bc0aa124e2525cbbc5fac1f75e73
Nic Cope <negz@rk0n.org>
2019-11-05 17:11:50 -0800

    Assume go generate tools are pinned using Go modules (or dep)
    The `generate` build tag is needed when using Go modules because the tool
    dependencies must be actual imports, so we want to make sure the file that
    imports the tools is not built under normal circumstances.
    
    https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- makelib/golang.mk
+++ makelib/golang.mk
@@ -81,8 +81,6 @@ DEP := $(TOOLS_HOST_DIR)/dep-$(DEP_VERSION)
 GOJUNIT := $(TOOLS_HOST_DIR)/go-junit-report
 GOCOVER_COBERTURA := $(TOOLS_HOST_DIR)/gocover-cobertura
 GOIMPORTS := $(TOOLS_HOST_DIR)/goimports
-CONTROLLER_GEN_VERSION ?= v0.2.1
-CONTROLLERGEN := $(TOOLS_HOST_DIR)/controller-gen
 
 GO := go
 GOHOST := GOOS=$(GOHOSTOS) GOARCH=$(GOHOSTARCH) go
@@ -133,6 +131,7 @@ endif
 
 GO_COMMON_FLAGS = $(GO_BUILDFLAGS) -tags '$(GO_TAGS)'
 GO_STATIC_FLAGS = $(GO_COMMON_FLAGS) $(GO_PKG_STATIC_FLAGS) -installsuffix static  -ldflags '$(GO_LDFLAGS)'
+GO_GENERATE_FLAGS = $(GO_BUILDFLAGS) -tags 'generate $(GO_TAGS)'
 
 export GO111MODULE
 
@@ -284,9 +283,9 @@ go.clean:
 go.distclean:
 	@rm -rf $(GO_VENDOR_DIR) $(GO_PKG_DIR)
 
-go.generate: $(GOIMPORTS) $(CONTROLLERGEN)
+go.generate: $(GOIMPORTS)
 	@$(INFO) go generate $(PLATFORM)
-	@CGO_ENABLED=0 CONTROLLERGEN=$(CONTROLLERGEN) $(GOHOST) generate $(GO_COMMON_FLAGS) $(GO_PACKAGES) $(GO_INTEGRATION_TEST_PACKAGES) || $(FAIL)
+	@CGO_ENABLED=0 $(GOHOST) generate $(GO_GENERATE_FLAGS) $(GO_PACKAGES) $(GO_INTEGRATION_TEST_PACKAGES) || $(FAIL)
 	@find $(GO_SUBDIRS) $(GO_INTEGRATION_TESTS_SUBDIRS) -type f -name 'zz_generated*' -exec $(GOIMPORTS) -l -w -local $(GO_PROJECT) {} \;
 	@$(OK) go generate $(PLATFORM)
 
@@ -389,16 +388,3 @@ $(GOCOVER_COBERTURA):
 	@GO111MODULE=off GOPATH=$(TOOLS_HOST_DIR)/tmp-gocover-cobertura GOBIN=$(TOOLS_HOST_DIR) $(GOHOST) get github.com/t-yuki/gocover-cobertura || rm -fr $(TOOLS_HOST_DIR)/tmp-covcover-cobertura || $(FAIL)
 	@rm -fr $(TOOLS_HOST_DIR)/tmp-gocover-cobertura
 	@$(OK) installing gocover-cobertura
-
-$(CONTROLLERGEN):
-	@$(INFO) installing controller-gen @$(CONTROLLER_GEN_VERSION)
-	@mkdir -p $(TOOLS_HOST_DIR)/tmp-controllergen || $(FAIL)
-
-	@# `go get` only supports versioned go packages when GO111MODULE=on 
-	@#  since $(TOOLS_HOST_DIR) is under $(GO_PROJECT), make a temp folder which has a go.mod, 
-	@#  so that $(GO_PROJECT)/go.mod doesn't get modified because of running `go get`
-	@cd $(TOOLS_HOST_DIR)/tmp-controllergen; rm -f go.mod; GO111MODULE=on $(GOHOST) mod init tmp-controllergen
-	@cd $(TOOLS_HOST_DIR)/tmp-controllergen; GOPATH=$(abspath $(GO_PKG_DIR)) GO111MODULE=on GOBIN=$(TOOLS_HOST_DIR) $(GOHOST) get sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_GEN_VERSION) || $(FAIL)
-	@rm -fr $(TOOLS_HOST_DIR)/tmp-controllergen
-
-	@$(OK) installing controller-gen @$(CONTROLLER_GEN_VERSION)
