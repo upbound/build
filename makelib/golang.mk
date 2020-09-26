1b402c19c4adc5049627be82b808ff8d57c815b8
Nic Cope <negz@rk0n.org>
2020-09-26 14:32:43 -0700

    Bump golangci-lint version
    I'd like to update our linting Github action(s), and the actions don't support
    this fairly old version of golangci-lint. I've tested the new version against
    crossplane, crossplane-runtime, and provider-gcp. It introduces some new lint
    rules that will need to be addressed, but otherwise works fine.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- makelib/golang.mk
+++ makelib/golang.mk
@@ -96,7 +96,7 @@ endif
 
 # We use a consistent version of golangci-lint to ensure everyone gets the same
 # linters.
-GOLANGCILINT_VERSION ?= 1.23.8
+GOLANGCILINT_VERSION ?= 1.31.0
 GOLANGCILINT := $(TOOLS_HOST_DIR)/golangci-lint-v$(GOLANGCILINT_VERSION)
 
 GO_BIN_DIR := $(abspath $(OUTPUT_DIR)/bin)
