b48c9c461d613152bbedd2ba9e9b5eb12cc0b2bd
hasheddan <georgedanielmangum@gmail.com>
2022-10-12 14:01:28 -0400

    Separate docs generation from publish and add run target
    Separates docs generation and publishing, and adds a run target that
    just calls make again on the docs repo run target.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- makelib/docs.mk
+++ makelib/docs.mk
@@ -38,7 +38,7 @@ DOCS_VERSION_DIR := $(DOCS_WORK_DIR)/$(DEST_DOCS_DIR)/$(DOCS_VERSION)
 # ====================================================================================
 # Targets
 
-docs.publish:
+docs.generate:
 	rm -rf $(DOCS_WORK_DIR)
 	mkdir -p $(DOCS_WORK_DIR)
 	git clone --depth=1 -b master $(DOCS_GIT_REPO) $(DOCS_WORK_DIR)
@@ -48,6 +48,11 @@ docs.publish:
 		cp -r $(SOURCE_DOCS_DIR)/ $(DOCS_VERSION_DIR); \
 		$(OK) Version included in documentation ; \
 	fi
+
+docs.run: docs.generate
+	cd $(DOCS_WORK_DIR) && DOCS_VERSION=$(DOCS_VERSION) $(MAKE) run
+
+docs.publish: docs.generate
 	cd $(DOCS_WORK_DIR) && DOCS_VERSION=$(DOCS_VERSION) $(MAKE) publish
 
 # ====================================================================================
